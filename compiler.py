8d87c882ac30c409924b9e3314fa4c73f8a36f99
Daniel Gaytan <dnigaytan07@gmail.com>
2020-04-24 12:48:54 -0500

    updated compiler.py and created operators_tokens and error_tokens

---
+++ compiler.py
@@ -0,0 +1,13 @@
+import operators_tokens as op
+import error_tokens as e
+
+
+class Compiler(object):
+
+    def __init__(self, name='default'):
+        self.name = name
+
+
+# Testing purposes
+if __name__ == '__main__':
+    compiler = Compiler()
