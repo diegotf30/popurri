990d807ec0e439941dbd8275919a737c629e09df
Daniel Gaytan <dnigaytan07@gmail.com>
2020-05-18 01:48:59 -0500

    Added memory reservation and update features

--- memory.py
+++ memory.py
@@ -1,7 +1,25 @@
 from popurri_memory_tokens import *
+import math
+
+# 9999 / 4 = 2499
+TYPE_OFFSET = 2499
+CONTEXT_TOKEN_OFFSET = 4
 
 
 class MemoryHandler():
+
+    dataTypes = {
+        'int': INT,
+        'float': FLOAT,
+        'bool': BOOL,
+        'string': STRING,
+    }
+
+    memoryContextStartingPoint = {
+        GLOBAL: 10000,
+        LOCAL: 20000,
+        TEMPORAL: 30000
+    }
 
     def __init__(self):
         '''
@@ -9,7 +27,25 @@ class MemoryHandler():
         '''
         self.mem_context_list = [Memory()] * 3
 
-    def updateMemory(self, address=None, value=None, dtype=None):
+    def getType(self, value=None):
+        return self.dataTypes[type(value)]
+
+    def reserveMemoryAddress(self, context=None, dtype=None):
+        '''
+        this function will reserve an address for the corresponding source call.
+
+        '''
+
+        quadrant_memory = self.memoryContextStartingPoint[context]
+
+        context -= CONTEXT_TOKEN_OFFSET
+
+        relative_memory = self.mem_context_list[context].updateTypeStackSize(
+            dtype)
+
+        return quadrant_memory + (TYPE_OFFSET * dtype) + relative_memory
+
+    def updateMemory(self, address=None, value=None):
         '''
         Updates a particular mem stack size/value
         this functions should be used only by PopurriListener.py
@@ -17,18 +53,30 @@ class MemoryHandler():
 
         context, address = self.getAddressContext(address)
 
+        # obtains the data type from the address [INT, FLOAT, BOOL, STRING]
+        dtype = math.floor(address / TYPE_OFFSET)
+
+        # obtains the relative address within context address stack [1 -> TYPE_OFFSET]
+        address -= (dtype * TYPE_OFFSET)
+
         # updates the corresponding type list
-        self.mem_context_list[context].updateTypeStackSize(dtype)
+        # self.mem_context_list[context].updateTypeStackSize(dtype)
 
         if value is not None:
             # inserts value in the corresponding type list
-            self.mem_context_list[context].updateAddress(
+            self.mem_context_list[context - CONTEXT_TOKEN_OFFSET].updateAddress(
                 address=address, dtype=dtype, value=value)
+
+        print(self.mem_context_list[context - CONTEXT_TOKEN_OFFSET].list_types)
+        return True
 
     def getAddressContext(self, address):
         '''
         given the address return the context [GLOBAL, LOCAL, TEMPORAL] 
         and removes the offset from the address [this new address is returned too]
+        10000 -> 19999 : GLOBAL
+        20000 -> 29999 : LOCAL
+        30000 -> 39999 : TEMPORAL
         '''
         if address >= 10000 and address <= 19999:
             return (GLOBAL, address - 10000)
@@ -51,23 +99,24 @@ class Memory():
         '''
         Lists size can be redefined after creating a Memory object.
         '''
-        self.list_types = [] * 4
+        self.list_types = [None] * 4
 
-        for dtype in [INT, FLOAT, BOOL, STRING]:
+        for dtype in range(0, 4):
             self.list_types[dtype] = []
 
     def updateTypeStackSize(self, dtype=None):
         '''
         Increase the list size of a certain type[INT, FLOAT, BOOL, STRING]
+        this function simulates the addition of another slot of a certain type
         '''
         if dtype is None:
             return False
 
         self.list_types[dtype].append(None)
 
-        return True
+        return len(self.list_types[dtype])
 
-    def getAddressStack(self):
+    def getContextStack(self):
         '''
         return a tuple of list_types
         '''
@@ -83,7 +132,7 @@ class Memory():
         if type is None or value is None:
             return False
 
-        self.list_types[dtype][address] = value
+        self.list_types[dtype][address-1] = value
 
         return True
 
